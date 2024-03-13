b0cfa66e7486f1f847a6407da643efe9313a63fb
AntonZimyanin <zimyanin88@gmail.com>
2024-03-13 21:46:53 +0300

    configure swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,13 +6,6 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-/**
- * @SWG\Tag(
- *   name="BaseController",
- *   description="Operations about base controller"
- * )
- */
-
 
 class Controller extends BaseController
 {
